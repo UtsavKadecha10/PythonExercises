b8c39360a9f6a8940cef916511881c5b0f18ffa5
Utsav <ut.kadecha@gmail.com>
2022-05-14 12:03:56 +0530

    Modified the files and commited

--- 4_StarPattern.py
+++ 4_StarPattern.py
@@ -1,15 +1,33 @@
-"""Exercise - 4"""
-"""Star Pattern"""
 """
-Pattern Printing
 Input = integer n
-Output:
+for n=4
+Output Boolean variable = True/False:
+True
 *
 **
 ***
 ****
+
+False
+****
+***
+**
+*
 """
 
 """SOLUTION"""
-"""Star Printing"""
+"""Bool Star Pattern"""
 
+print("Enter n")
+n=int(input())
+print("Enter Boolean character")
+i=bool(input())
+if i:
+    for x in range(1, n+1):
+        for n in range(x):
+            print("*", end="")
+else:
+    for x in range(n, 0, -1):
+        for n in range(x):
+            print("*", end="")    
+print("\n")
